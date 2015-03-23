c4e552d398fc2c60b21e08f09394726c7910bb9f
Zihang Chen <chsc4698@gmail.com>
2015-03-22 22:42:49 -0700

    add lang conf for clojure

---
+++ code/judge/langs/clojure.py
@@ -0,0 +1,15 @@
+from ._fundamental import def_interpreted_lang
+from .java import JAVA_SECURITY_POLICY_PATH
+
+CLJ_JAR_PATH = '/home/wo/clojure-1.6.0/clojure-1.6.0.jar'
+
+def_interpreted_lang(id_=5,
+                     canonical_name='Clojure',
+                     suffix='clj',
+                     exec_cmd=['java', '-cp', CLJ_JAR_PATH,
+                               'clojure.main', '{source}',
+                               '-Djava.security.manager',
+                               '-Djava.security.policy=%s' %
+                               JAVA_SECURITY_POLICY_PATH],
+                     memory_limit_multiplier=3,
+                     time_limit_multiplier=3)
